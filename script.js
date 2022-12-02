e04c9c8f6f292cd60e9bf12a4e7e6ade4e485b01
qxan <mehmet.enfa@gmail.com>
2022-12-02 23:58:43 +0300

    function 2

--- script.js
+++ script.js
@@ -62,4 +62,19 @@ function generatePassword(lower, upper, number, symbol, length) {
   return finalPassword;
 }
 
+function getRandomLower() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+}
 
+function getRandomUpper() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+}
+
+function getRandomNumber() {
+  return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+}
+
+function getRandomSymbol() {
+  const symbols = "!@#$%^&*(){}[]=<>/,.";
+  return symbols[Math.floor(Math.random() * symbols.length)];
+}
